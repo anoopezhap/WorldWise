b693ab02e7101a5f516a97bd76567b2593bc2f6e
anoopezhap <anoopezhap@gmail.com>
2023-10-16 21:37:39 -0400

    myfirst commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFount from "./pages/PageNotFount";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
+
+        <Route path="app" element={<AppLayout />} />
+
+        <Route path="*" element={<PageNotFount />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
